7b4aa150f2b83d1b762cde75b653b567382ff368
Rafal Lakomski <rafal.lakomski.qa@gmail.com>
2017-08-06 00:02:21 +0200

    formatted code for MyFirstProgram

--- sandbox/src/main/java/rl/java/course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/rl/java/course/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package rl.java.course.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
-}
 }
