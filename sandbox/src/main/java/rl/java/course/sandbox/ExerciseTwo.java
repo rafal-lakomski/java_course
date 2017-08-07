2cc9760aa72db28fc2128edbd94b485cdc636742
Rafal Lakomski <rafal.lakomski.qa@gmail.com>
2017-08-07 21:38:45 +0200

    added exercise two

---
+++ sandbox/src/main/java/rl/java/course/sandbox/ExerciseTwo.java
@@ -0,0 +1,13 @@
+package rl.java.course.sandbox;
+
+public class ExerciseTwo {
+
+  public static void main(String[] args) {
+    Point p = new Point(2,5, 5, 9);
+    System.out.println("Distance between Point1 ("
+            + p.x1 + "," + p.y1 + ")" + " and Point2 ("
+           + p.x2 + "," + p.y2 + ")" + " = " + p.distance());
+  }
+  }
+
+
