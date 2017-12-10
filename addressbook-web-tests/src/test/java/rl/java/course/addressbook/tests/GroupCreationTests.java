b7981810f86ee8edce16bc96f185d6814221e1ea
Rafal Lakomski <rafal.lakomski.qa@gmail.com>
2017-12-10 21:45:34 +0100

    Sorting L4 M10

--- addressbook-web-tests/src/test/java/rl/java/course/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/rl/java/course/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import rl.java.course.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 
@@ -15,12 +14,14 @@ public class  GroupCreationTests extends TestBase {
   public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test2", null, null);
+        GroupData group = new GroupData("test1", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
-        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
 }
          }
