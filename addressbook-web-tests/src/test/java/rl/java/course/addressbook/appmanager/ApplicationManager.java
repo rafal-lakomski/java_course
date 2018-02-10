dfb40df60a948aba836134171a954ff559690410
Rafal Lakomski <rafal.lakomski.qa@gmail.com>
2018-02-10 17:19:59 +0100

    added properties

--- addressbook-web-tests/src/test/java/rl/java/course/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/rl/java/course/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -20,9 +26,12 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/$s.properties", target))));
     if (Objects.equals(browser, BrowserType.FIREFOX)) {
       System.setProperty("webdriver.gecko.driver", "C:/Geckodriver/geckodriver.exe");
       wd = new FirefoxDriver();
@@ -34,12 +43,12 @@ public class ApplicationManager {
       wd = new EdgeDriver();
     }
     wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login (properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
--- addressbook-web-tests/src/test/java/rl/java/course/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/rl/java/course/addressbook/tests/TestBase.java
@@ -7,7 +7,8 @@ import rl.java.course.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
